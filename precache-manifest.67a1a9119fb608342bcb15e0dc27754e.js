1568a27233e85f75c4822b3dacd155d2983fb7b8
farid.mahmud <farid.mahmud@tonjoo.com>
2019-08-05 13:08:24 +0700

    Updates

---
+++ precache-manifest.67a1a9119fb608342bcb15e0dc27754e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd594e1c0b03857e58b1faa9dff8eaab",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ba4903b8c82024ce082c",
+    "url": "/monster-rolodex/static/css/main.389f305a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ba4903b8c82024ce082c",
+    "url": "/monster-rolodex/static/js/main.82f14805.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
